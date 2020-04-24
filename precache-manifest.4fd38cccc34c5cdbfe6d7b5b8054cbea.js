0ae06607d8ae4b32b8065155a5f92fed84e10bc4
shingaryu <shingaryu@gmail.com>
2020-04-25 08:44:14 +0900

    Updates

---
+++ precache-manifest.4fd38cccc34c5cdbfe6d7b5b8054cbea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb295d5d2755d075cd97eebfb1427fa3",
+    "url": "/combination-calculator/index.html"
+  },
+  {
+    "revision": "95247f8e41ba9d2a0cf3",
+    "url": "/combination-calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b6de79f9519afbd9ebb4",
+    "url": "/combination-calculator/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "95247f8e41ba9d2a0cf3",
+    "url": "/combination-calculator/static/js/2.b45ff9b7.chunk.js"
+  },
+  {
+    "revision": "318bf8be97efe43d24de8297746c326a",
+    "url": "/combination-calculator/static/js/2.b45ff9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6de79f9519afbd9ebb4",
+    "url": "/combination-calculator/static/js/main.d75d6b9b.chunk.js"
+  },
+  {
+    "revision": "1211d447cd61159e09e1",
+    "url": "/combination-calculator/static/js/runtime-main.3e513783.js"
+  },
+  {
+    "revision": "b4b7486aac9c953bb2905a8a6579d188",
+    "url": "/combination-calculator/static/media/strategies.b4b7486a.csv"
+  },
+  {
+    "revision": "d04147c7096037bda9e7435192674a9b",
+    "url": "/combination-calculator/static/media/strength-table.d04147c7.csv"
+  },
+  {
+    "revision": "706fcf81ecf091e2e964ddc4e20e1424",
+    "url": "/combination-calculator/static/media/usage.706fcf81.csv"
+  }
+]);
