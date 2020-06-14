fa73755ce585eac27a81a6c70a9b597ef3ae3d9f
shingaryu <shingaryu@gmail.com>
2020-06-14 22:05:08 +0900

    Remove the function of the table row selection

--- src/components/searchResultComponent.jsx
+++ src/components/searchResultComponent.jsx
@@ -6,23 +6,9 @@ import { I18nContext } from 'react-i18next';
 export class SearchResultComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedRow: null
-    };
   }
 
   static contextType = I18nContext;
-
-  onClickTableRow(rowIndex, pokemonIds) {
-    if (this.state.selectedRow === rowIndex) {
-      this.setState({ selectedRow: null });
-      this.props.onSelectChange(null);
-    } else {
-      this.setState({ selectedRow: rowIndex })
-      this.props.onSelectChange(pokemonIds);
-    }
-
-  }
 
   render() {
     const t = this.context.i18n.t.bind(this.context.i18n);
@@ -40,7 +26,7 @@ export class SearchResultComponent extends React.Component {
           <Table striped bordered hover size="sm">
             <thead>
               <tr>
-                <th key='h-i'>#</th>
+                <th key='h-i'>{t('search.columnRank')}</th>
                 {this.props.searchResult.length===0?
                   <th key={`h-p0`}>Pokemon</th>:
                   this.props.searchResult[0].pokemonIds.map((x, i) => <th key={`h-p${i}`}>{t('search.columnPokemon')}</th>)
@@ -50,8 +36,7 @@ export class SearchResultComponent extends React.Component {
             </thead>
             <tbody>
               {this.props.searchResult.map((result, index) => (
-                <tr key={index} onClick={() => this.onClickTableRow(index, result.pokemonIds)} 
-                  className={this.state.selectedRow===index?'cursor-pointer selected-row':'cursor-pointer'}>
+                <tr key={index}>
                   <td key={`${index}-i`}>{index + 1}</td>
                   {result.pokemonNames.map((x, i) => <td key={`${index}-p${i}`}>{x}</td>)}
                   <td key={`${index}-v`}>{result.value.toFixed(4)}</td>
