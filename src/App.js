c89f3365a748812bca113c40b48c1169d35f6dc7
shingaryu <shingaryu@gmail.com>
2020-06-14 20:38:00 +0900

    Set the maximum width of the entire app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
-// import './App.css';
-import { Navbar } from 'react-bootstrap';
+import './App.css';
+import { Navbar, Container } from 'react-bootstrap';
 import { TeamBuilderComponent } from './components/teamBuilderComponent';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'
@@ -10,10 +10,12 @@ function App() {
     <Suspense fallback={null}>
       <I18nextProvider i18n={i18n}>
         <div className="App">
+        <Container fluid>
           <Navbar bg="light">
             <Navbar.Brand>Pokémon VGC Team Combination Calculator</Navbar.Brand>
           </Navbar>
           <TeamBuilderComponent></TeamBuilderComponent>
+        </Container>
         </div>
       </I18nextProvider>
     </Suspense>
