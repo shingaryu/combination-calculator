4da3444bf5e56fa5750884523f1493c6b6823e7a
shingaryu <shingaryu@gmail.com>
2020-07-14 20:02:30 +0900

    update stringSanitizer and translations to accept new pokemon

--- src/services/stringSanitizer.js
+++ src/services/stringSanitizer.js
@@ -5,9 +5,12 @@ export function translateSpeciesIfPossible(text, t) {
   if (text.indexOf('-Galar') >= 0) {
     species = text.substring(0, text.indexOf('-Galar'));
     suffix = '(ガラル)';
-  } 
+  } else if (text.indexOf('-Alola') >= 0) {
+    species = text.substring(0, text.indexOf('-Alola'));
+    suffix = '(アローラ)';
+  }
 
-  const sanitizedKey = 'pokemon.species.' + species.replace(':', '').replace("'", '').replace('.', '').replace(' ', '').replace('-', '').toLowerCase();
+  const sanitizedKey = 'pokemon.species.' + species.replace(':', '').replace("'", '').replace('.', '').replace(' ', '').replace('-', '').replace('-', '').toLowerCase();
   const translated = t(sanitizedKey);
   if (translated === sanitizedKey) { // maybe missing key
     return text;
