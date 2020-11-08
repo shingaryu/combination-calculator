c7eeda360a100e870e4772c7b844f9f9d10c2065
shingaryu <shingaryu@gmail.com>
2020-11-08 23:47:39 +0100

    Now show selection evaluation when team length is less than 3

--- src/components/statisticalEvaluation.tsx
+++ src/components/statisticalEvaluation.tsx
@@ -16,52 +16,10 @@ type StatisticalEvaluationState = {
 }
 
 class StatisticalEvaluationRaw extends React.Component<StatisticalEvaluationProps, StatisticalEvaluationState> {
-  constructor(props: StatisticalEvaluationProps) {
-    super(props);
-  }
 
   render() {   
     const t = this.props.t;
-
     const mmopCalculator = new MMOPCalculator();
-
-    const statisticsExp = mmopCalculator.evaluateTeamSelections(this.props.myTeam, this.props.sortedPokemonList, t);
-    const graphLabelsExp = statisticsExp.map(x => x.selectionShortName);
-    const fullStrListExp = statisticsExp.map(x => x.selectionFullName);
-    const graphDataSetsExp = [
-      {
-        dataLabel: t('graph.selectionEvaluation.legend'),
-        values: statisticsExp.map(x => parseInt(x.value.toFixed(0))),
-        colorRGB: [200, 99, 132]
-      }
-    ];
-
-    const toolTipOptionsExp = (isVertical: boolean) => ({
-      tooltips: {
-        callbacks: {
-          label: (tooltipItem: any, data: any) => {
-            const fullLabel = fullStrListExp[tooltipItem.index];
-            var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-            if (label) {
-                label += ': ';
-            }
-            label += isVertical? tooltipItem.yLabel: tooltipItem.xLabel;
-            return [fullLabel, label];
-        }
-        }
-      }
-    });
-
-    const staticticsIndExp = mmopCalculator.evaluateTeamIndividuals(this.props.myTeam, this.props.sortedPokemonList);
-    const graphLabelsIndExp = staticticsIndExp.map(x => translateSpeciesIfPossible(x.pokemon.species, t));
-    const graphDataSetsIndExp = [
-      {
-        dataLabel: t('graph.individualEvaluation.legend'),
-        values: staticticsIndExp.map(x => parseInt(x.value.toFixed(0))),
-        colorRGB: [32, 99, 132]
-      }
-    ];
 
     const opponentMaximums = mmopCalculator.evaluateTeamToTargetIndividuals(this.props.myTeam, this.props.sortedPokemonList);
     opponentMaximums.matchups.sort((a, b) => {
@@ -84,6 +42,52 @@ class StatisticalEvaluationRaw extends React.Component<StatisticalEvaluationProp
       }
     ];
 
+    let graphLabelsExp: string[] = [];
+    let graphDataSetsExp: any[] = [];
+    let toolTipOptionsExp: (isVertical: boolean) => any = (isVertical: boolean) => {};
+
+    let graphLabelsIndExp: string[] = [];
+    let graphDataSetsIndExp: any[] = [];
+    
+    if (this.props.myTeam.length >= 3) {
+      const statisticsExp = mmopCalculator.evaluateTeamSelections(this.props.myTeam, this.props.sortedPokemonList, t);
+      graphLabelsExp = statisticsExp.map(x => x.selectionShortName);
+      const fullStrListExp = statisticsExp.map(x => x.selectionFullName);
+      graphDataSetsExp = [
+        {
+          dataLabel: t('graph.selectionEvaluation.legend'),
+          values: statisticsExp.map(x => parseInt(x.value.toFixed(0))),
+          colorRGB: [200, 99, 132]
+        }
+      ];
+  
+      toolTipOptionsExp = (isVertical: boolean) => ({
+        tooltips: {
+          callbacks: {
+            label: (tooltipItem: any, data: any) => {
+              const fullLabel = fullStrListExp[tooltipItem.index];
+              var label = data.datasets[tooltipItem.datasetIndex].label || '';
+  
+              if (label) {
+                  label += ': ';
+              }
+              label += isVertical? tooltipItem.yLabel: tooltipItem.xLabel;
+              return [fullLabel, label];
+          }
+          }
+        }
+      });
+  
+      const staticticsIndExp = mmopCalculator.evaluateTeamIndividuals(this.props.myTeam, this.props.sortedPokemonList);
+      graphLabelsIndExp = staticticsIndExp.map(x => translateSpeciesIfPossible(x.pokemon.species, t));
+      graphDataSetsIndExp = [
+        {
+          dataLabel: t('graph.individualEvaluation.legend'),
+          values: staticticsIndExp.map(x => parseInt(x.value.toFixed(0))),
+          colorRGB: [32, 99, 132]
+        }
+      ];
+    }
 
     return (
       <>
@@ -98,29 +102,33 @@ class StatisticalEvaluationRaw extends React.Component<StatisticalEvaluationProp
               valueMin={-256} valueMax={1024} valueStep={256} />          
           </Col>
         </Row>
-        <Row className="mt-5">
-          <Col>
-            <h4>{t('graph.selectionEvaluation.title')}</h4>
-            <div className="description-box mb-4" >
-              <div dangerouslySetInnerHTML={{__html: t('graph.selectionEvaluation.description')}} />
-              <div className="tips">・{t('graph.selectionEvaluation.tips1')}</div>
-              <div className="tips">・{t('graph.selectionEvaluation.tips2')}</div>
-            </div>
-            <GraphComponent labels={graphLabelsExp} datasets={graphDataSetsExp} heightVertical={300} widthVertical={800}
-              valueMin={-32} valueMax={64} valueStep={32} optionsBar={toolTipOptionsExp(true)} optionsHorizontal={toolTipOptionsExp(false)} />
-          </Col>
-        </Row>        
-        <Row className="mt-5">
-          <Col>
-            <h4>{t('graph.individualEvaluation.title')}</h4>
-            <div className="description-box mb-4" >
-              <div dangerouslySetInnerHTML={{__html: t('graph.individualEvaluation.description')}} />
-              <div className="tips">・{t('graph.individualEvaluation.tips1')}</div>
-            </div>
-            <GraphComponent labels={graphLabelsIndExp} datasets={graphDataSetsIndExp} heightVertical={300} widthVertical={800} 
-              valueMin={-128} valueMax={256} valueStep={128} xTicksRotation={0}/>
-          </Col>
-        </Row>        
+        { this.props.myTeam.length >= 3 &&
+          <>
+            <Row className="mt-5">
+              <Col>
+                <h4>{t('graph.selectionEvaluation.title')}</h4>
+                <div className="description-box mb-4" >
+                  <div dangerouslySetInnerHTML={{__html: t('graph.selectionEvaluation.description')}} />
+                  <div className="tips">・{t('graph.selectionEvaluation.tips1')}</div>
+                  <div className="tips">・{t('graph.selectionEvaluation.tips2')}</div>
+                </div>
+                <GraphComponent labels={graphLabelsExp} datasets={graphDataSetsExp} heightVertical={300} widthVertical={800}
+                  valueMin={-32} valueMax={64} valueStep={32} optionsBar={toolTipOptionsExp(true)} optionsHorizontal={toolTipOptionsExp(false)} />
+              </Col>
+            </Row>        
+            <Row className="mt-5">
+              <Col>
+                <h4>{t('graph.individualEvaluation.title')}</h4>
+                <div className="description-box mb-4" >
+                  <div dangerouslySetInnerHTML={{__html: t('graph.individualEvaluation.description')}} />
+                  <div className="tips">・{t('graph.individualEvaluation.tips1')}</div>
+                </div>
+                <GraphComponent labels={graphLabelsIndExp} datasets={graphDataSetsIndExp} heightVertical={300} widthVertical={800} 
+                  valueMin={-128} valueMax={256} valueStep={128} xTicksRotation={0}/>
+              </Col>
+            </Row>       
+          </> 
+        }
       </>
     )
 
